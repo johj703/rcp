7ff35cdaba0c6a59bcedacf4f848a769b2e64281
Jo <johj703@nate.com>
2023-03-31 21:39:32 +0900

    가짜 데이터 만들기

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,9 @@
 module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
   singleQuote: true,
+  trailingComma: 'none',
+  allowParens: 'avoid',
   semi: false,
+  printWidth: 90,
 }
