a041add30281f6bedff10a8c2407f3ea1a5ac86f
Jo <johj703@nate.com>
2023-04-04 14:39:09 +0900

    이벤트 리스너 이해하기

---
+++ ch02/ch02_5/src/App.tsx
@@ -0,0 +1,29 @@
+import DispatchEvent from './pages/DispatchEvent'
+import DragDrop from './pages/DragDrop'
+import EventBubbling from './pages/EventBubbling'
+import EventListener from './pages/EventListener'
+import FileDrop from './pages/FileDrop'
+import FileInput from './pages/FileInput'
+import OnChange from './pages/OnChange'
+import OnClick from './pages/OnClick'
+import ReactOnClick from './pages/ReactOnClick'
+import StopPropagation from './pages/StopPropagation'
+import VariousInputs from './pages/VariousInputs'
+
+export default function App() {
+  return (
+    <div>
+      <FileDrop />
+      <DragDrop />
+      <FileInput />
+      <OnChange />
+      <VariousInputs />
+      <StopPropagation />
+      <EventBubbling />
+      <DispatchEvent />
+      <ReactOnClick />
+      <OnClick />
+      <EventListener />
+    </div>
+  )
+}
