4707562b419c1c06f0b5028c3c12c92bcb97fe9f
Jo <johj703@nate.com>
2023-04-04 13:49:47 +0900

    PropsWithChildren 타입과 Children 속성

--- ch02/ch02_4/src/P.tsx
+++ ch02/ch02_4/src/P.tsx
@@ -1,11 +1,7 @@
-import type {FC, ReactNode} from 'react'
+import type {FC, PropsWithChildren} from 'react'
 
-export type PProps = {
-  children?: ReactNode
+export type PProps = {}
+const P: FC<PropsWithChildren<PProps>> = (props) => {
+  return <p {...props} />
 }
-const P: FC<PProps> = (props) => {
-  const {children} = props
-  return <p children={children} />
-}
-
 export default P
