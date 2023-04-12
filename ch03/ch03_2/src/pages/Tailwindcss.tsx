e1e69251bc69101c6098708411e32ff85d12358c
Jo <johj703@nate.com>
2023-04-12 17:48:07 +0900

    텍스트 관련 컴포넌트 구현하기

--- ch03/ch03_2/src/pages/Tailwindcss.tsx
+++ ch03/ch03_2/src/pages/Tailwindcss.tsx
@@ -6,7 +6,7 @@ export default function Tailwindcss() {
       <p className="w-full p-4 text-3xl text-white">Tailwindcss</p>
       <p className="italic text-gray-50 line-clamp-3">{D.randomParagraphs(10)}</p>
       <button className="btn btn-primary" style={{textTransform: 'none'}}>
-        button
+        Button
       </button>
     </div>
   )
