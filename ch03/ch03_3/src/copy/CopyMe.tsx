04c917ea309aa4025002371b24bd3f3e0ebe138f
Jo <johj703@nate.com>
2023-04-13 22:12:48 +0900

    CSS 상자 모델 이해하기 세팅

---
+++ ch03/ch03_3/src/copy/CopyMe.tsx
@@ -0,0 +1,10 @@
+import {Title} from '../components'
+
+export default function CopyMe() {
+  return (
+    <section>
+      <Title>CopyMe</Title>
+      <div className="mt-4"></div>
+    </section>
+  )
+}
