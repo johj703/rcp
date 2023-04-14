e2b05f5edf67fc18acbe1fb95586f16c20bf028b
Jo <johj703@nate.com>
2023-04-14 10:13:45 +0900

    Div 컴포넌트 구현하기2

--- ch03/ch03_3/src/pages/DivTest.tsx
+++ ch03/ch03_3/src/pages/DivTest.tsx
@@ -1,10 +1,14 @@
-import {Title} from '../components'
+import {Title, Subtitle, Div, Icon} from '../components'
 
-export default function CopyMe() {
+export default function DivTest() {
   return (
-    <section>
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+    <section className="mt-4">
+      <Title>DivTest</Title>
+      {/* <Div className="text-center text-blue-100 bg-blue-600" height="6rem"> */}
+      <Div className="text-center text-blue-100 bg-blue-600">
+        <Icon name="home" className="text-3xl" />
+        <Subtitle>Home</Subtitle>
+      </Div>
     </section>
   )
 }
