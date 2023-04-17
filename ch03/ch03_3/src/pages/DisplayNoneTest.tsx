8c82c7a1b29cac376900150f5a672776003fe6c0
Jo <johj703@nate.com>
2023-04-17 12:47:43 +0900

    visibility 스타일 속성 - none 속성값 테스트

--- ch03/ch03_3/src/pages/DisplayNoneTest.tsx
+++ ch03/ch03_3/src/pages/DisplayNoneTest.tsx
@@ -1,10 +1,14 @@
 import {Title} from '../components'
 
-export default function CopyMe() {
+export default function DisplayNoneTest() {
   return (
-    <section>
-      <Title>CopyMe</Title>
-      <div className="mt-4"></div>
+    <section className="mt-4">
+      <Title>DisplayNoneTest</Title>
+      <div className="mt-4">
+        <p className="visible">visiblility: visible text</p>
+        <p className="invisible">visibility: hidden text</p>
+        <p className="hidden">display: none text</p>
+      </div>
     </section>
   )
 }
