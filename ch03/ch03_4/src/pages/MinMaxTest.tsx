e23ad15a56137076576aebf81dbfcffc94ca6fc9
Jo <johj703@nate.com>
2023-04-19 20:28:12 +0900

    min-width와 max-width 스타일 속성

--- ch03/ch03_4/src/pages/MinMaxTest.tsx
+++ ch03/ch03_4/src/pages/MinMaxTest.tsx
@@ -1,10 +1,17 @@
-import {Title} from '../components'
+import {Div, Title} from '../components'
+import * as D from '../data'
 
 export default function MinMaxTest() {
+  // prettier-ignore
   return (
-    <section>
+    <section className="mt-4">
       <Title>MinMaxTest</Title>
-      <div className="mt-4"></div>
+      <Div className="mt-4 bg-gray-300">
+        <Div src={D.randomImage(800, 300)} className="bg-cover">
+          <Div className="w-1/2 bg-blue-500 h-80"
+            minWidth="300px" maxWidth="500px" />
+        </Div>
+      </Div>
     </section>
   )
 }
