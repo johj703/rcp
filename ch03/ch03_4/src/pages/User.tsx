4fe77c2e43e7ce9d0ce784ea212b6f9cad180fbb
Jo <johj703@nate.com>
2023-04-20 22:07:11 +0900

    User 컴포넌트 만들기

--- ch03/ch03_4/src/pages/User.tsx
+++ ch03/ch03_4/src/pages/User.tsx
@@ -1,10 +1,22 @@
-import {Title} from '../components'
+import type {FC} from 'react'
+import type {DivProps} from '../components'
+import * as D from '../data'
+import {Div, Avatar} from '../components'
 
-export default function User() {
+export type UserProps = DivProps & {
+  user: D.IUser
+}
+const User: FC<UserProps> = ({user, ...props}) => {
+  const {name, email, jobTitle, avatar} = user
   return (
-    <section>
-      <Title>User</Title>
-      <div className="mt-4"></div>
-    </section>
+    <Div {...props}>
+      <div className="flex p-2">
+        <Avatar src={avatar} size="2rem" />
+        <div className="ml-2">
+          <p className="font-bold">{name}</p>
+        </div>
+      </div>
+    </Div>
   )
 }
+export default User
