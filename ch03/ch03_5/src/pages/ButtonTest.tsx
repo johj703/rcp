8dd97a3b8f76c8383fbcc50a7ec254303f0c7ea5
Jo <johj703@nate.com>
2023-04-21 13:08:42 +0900

    daisyui CSS 컴포넌트 이해하기

---
+++ ch03/ch03_5/src/pages/ButtonTest.tsx
@@ -0,0 +1,8 @@
+export default function ButtonTest() {
+  return (
+    <section className="mt-4">
+      <h2 className="text-5xl font-bold text-center">ButtonTest</h2>
+      <div className="mt-4"></div>
+    </section>
+  )
+}
