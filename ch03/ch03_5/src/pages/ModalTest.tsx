7597ef9c94dd037b1f53675341496b743a8cdfc1
Jo <johj703@nate.com>
2023-04-24 17:20:19 +0900

    모달 컴포넌트 구현하기

--- ch03/ch03_5/src/pages/ModalTest.tsx
+++ ch03/ch03_5/src/pages/ModalTest.tsx
@@ -1,8 +1,31 @@
+import {Title, Subtitle} from '../components'
+import {Modal, ModalContent, ModalAction, Button} from '../theme/daisyui'
+import * as D from '../data'
+
 export default function ModalTest() {
+  const open = true // 혹은 false
+  const closeClicked = () => alert('closeClicked')
+  const acceptClicked = () => alert('acceptClicked')
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">ModalTest</h2>
-      <div className="mt-4"></div>
+      <Title>ModalTest</Title>
+      <Modal open={open}>
+        <ModalContent onCloseIconClicked={closeClicked}>
+          <Subtitle>Modal</Subtitle>
+          <p className="mt-4 text-justify">{D.randomParagraphs()}</p>
+          <ModalAction>
+            <Button
+              className="w-24 normal-case btn-primary btn-sm"
+              onClick={acceptClicked}>
+              Accept
+            </Button>
+            <Button className="w-24 normal-case btn-sm" onClick={closeClicked}>
+              Close
+            </Button>
+          </ModalAction>
+        </ModalContent>
+      </Modal>
     </section>
   )
 }
