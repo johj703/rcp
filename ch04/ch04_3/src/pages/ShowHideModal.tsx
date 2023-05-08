cd3beb1cc14634c3dd729d17b1787db50fde5d4d
Jo <johj703@nate.com>
2023-05-08 21:49:59 +0900

    useToggle 커스텀 훅 만들기

--- ch04/ch04_3/src/pages/ShowHideModal.tsx
+++ ch04/ch04_3/src/pages/ShowHideModal.tsx
@@ -1,8 +1,37 @@
+import {useCallback} from 'react'
+import {Title, Subtitle} from '../components'
+import {Button, Modal, ModalContent, ModalAction} from '../theme/daisyui'
+import {useToggle} from '../hooks'
+import * as D from '../data'
+
 export default function ShowHideModal() {
+  const [open, toggleOpen] = useToggle(false)
+  const onAccept = useCallback(() => {
+    toggleOpen()
+  }, [toggleOpen])
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">ShowHideModal</h2>
-      <div className="mt-4"></div>
+      <Title>ShowHideModal</Title>
+      <div className="flex justify-center p-4">
+        <Button className="btn-primary" onClick={toggleOpen}>
+          open modal
+        </Button>
+      </div>
+      <Modal open={open}>
+        <ModalContent onCloseIconClicked={toggleOpen}>
+          <Subtitle>Modal </Subtitle>
+          <p>{D.randomParagraphs()}</p>
+          <ModalAction>
+            <button className="btn btn-primary" onClick={onAccept}>
+              Accept
+            </button>
+            <button className="btn" onClick={toggleOpen}>
+              Close
+            </button>
+          </ModalAction>
+        </ModalContent>
+      </Modal>
     </section>
   )
 }
