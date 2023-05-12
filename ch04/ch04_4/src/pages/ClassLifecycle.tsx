94f43aa84e50721b9c5202fff5eda1e7dbba6d43
Jo <johj703@nate.com>
2023-05-12 13:16:03 +0900

    클래스 컴포넌트의 언마운트 - 1초마다 갱신하는 시계 만들기

--- ch04/ch04_4/src/pages/ClassLifecycle.tsx
+++ ch04/ch04_4/src/pages/ClassLifecycle.tsx
@@ -2,12 +2,24 @@ import {Component} from 'react'
 import {Title} from '../components'
 
 export default class ClassLifecycle extends Component {
+  state = {
+    today: new Date(),
+    intervalId: null as unknown as NodeJS.Timer // 타입스크립트가 요구하는 구현 방식
+  }
+  componentDidMoudt() {
+    const duration = 1000
+    const intervalId = setInterval(() => this.setState({today: new Date()}), duration)
+    this.setState({intervalId})
+  }
+  componentWillUnmount() {
+    clearInterval(this.state?.intervalId)
+  }
   render() {
-    const today = new Date()
+    const {today} = this.state
     return (
       <section className="mt-4">
         <Title>ClassLifecycle</Title>
-        <div className="mt-4">
+        <div className="flex flex-col items-center mt-4">
           <p className="font-mono text-3xl">{today.toLocaleDateString()}</p>
           <p className="font-mono text-3xl">{today.toLocaleTimeString()}</p>
         </div>
