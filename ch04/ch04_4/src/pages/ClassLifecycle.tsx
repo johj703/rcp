97fb3d94e4baa4bf4293d8413f4ec734db389d4a
Jo <johj703@nate.com>
2023-05-12 10:34:41 +0900

    useEffect와 useLayoutEffect 훅 이해하기 세팅

---
+++ ch04/ch04_4/src/pages/ClassLifecycle.tsx
@@ -0,0 +1,8 @@
+export default function ClassLifecycle() {
+  return (
+    <section className="mt-4">
+      <h2 className="text-5xl font-bold text-center">ClassLifecycle</h2>
+      <div className="mt-4"></div>
+    </section>
+  )
+}
---
+++ ch04/ch04_4/src/pages/WindowResizeTest.tsx
@@ -0,0 +1,8 @@
+export default function WindowResizeTest() {
+  return (
+    <section className="mt-4">
+      <h2 className="text-5xl font-bold text-center">WindowResizeTest</h2>
+      <div className="mt-4"></div>
+    </section>
+  )
+}
