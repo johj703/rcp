1e26245c6882fac7911c9ab927089f3d34ce49e7
Jo <johj703@nate.com>
2023-05-12 14:02:50 +0900

    useWindowResize 커스텀 훅 만들기

--- ch04/ch04_4/src/pages/WindowResizeTest.tsx
+++ ch04/ch04_4/src/pages/WindowResizeTest.tsx
@@ -1,8 +1,15 @@
+import {Title, Subtitle} from '../components'
+import {useWindowResize} from '../hooks'
+
 export default function WindowResizeTest() {
+  const [width, height] = useWindowResize()
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">WindowResizeTest</h2>
-      <div className="mt-4"></div>
+      <Title>WindowResizeTest</Title>
+      <Subtitle className="mt-4">
+        width: {width}, height: {height}
+      </Subtitle>
     </section>
   )
 }
