6cb817560871f85d8bdba038173b18fb1a17cfaa
Jo <johj703@nate.com>
2023-05-18 17:05:36 +0900

    이미지 파일을 대상으로 [열기] 대화 상자 띄우기

--- ch04/ch04_5/src/pages/ClickTest.tsx
+++ ch04/ch04_5/src/pages/ClickTest.tsx
@@ -1,8 +1,19 @@
+import {useRef, useCallback} from 'react'
+import {Title} from '../components'
+
 export default function ClickTest() {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const onClick = useCallback(() => inputRef.current?.click(), [])
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">ClickTest</h2>
-      <div className="mt-4"></div>
+      <Title>ClickTest</Title>
+      <div className="flex items-center justify-center mt-4">
+        <button className="mr-4 btn btn-primary" onClick={onClick}>
+          Click Me
+        </button>
+        <input ref={inputRef} className="hidden" type="file" accept="image/*" />
+      </div>
     </section>
   )
 }
