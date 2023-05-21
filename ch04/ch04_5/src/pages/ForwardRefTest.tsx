6aa10ad7239330619dab6510d9c60f6992c439e3
Jo <johj703@nate.com>
2023-05-21 16:40:20 +0900

    forwardRef 함수 이해하기

--- ch04/ch04_5/src/pages/ForwardRefTest.tsx
+++ ch04/ch04_5/src/pages/ForwardRefTest.tsx
@@ -1,8 +1,25 @@
-export default function ForwardRefTest() {
+import {useRef, useCallback, useEffect} from 'react'
+import {Title} from '../components'
+import {Input} from '../theme/daisyui'
+
+export default function InputFocusTest() {
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const getValue = useCallback(() => alert(`input value: ${inputRef.current?.value}`), [])
+
+  useEffect(() => inputRef.current?.focus(), [])
+  // prettier-ignore
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">ForwardRefTest</h2>
-      <div className="mt-4"></div>
+      <Title>ForwardRefTest</Title>
+      <div className="flex justify-center mt-4">
+        <div className="flex flex-col w-1/3 p-2">
+          <Input ref={inputRef} className="input-primary" />
+          <button onClick={getValue} className="mt-4 btn btn-primary">
+            get value
+          </button>
+        </div>
+      </div>
     </section>
   )
 }
