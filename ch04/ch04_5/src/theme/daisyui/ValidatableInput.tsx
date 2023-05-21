40d6fde2f675af5a2fdecbd800dbb3515713b190
Jo <johj703@nate.com>
2023-05-21 20:16:48 +0900

    ValidatableInput 컴포넌트 완성하기

--- ch04/ch04_5/src/theme/daisyui/ValidatableInput.tsx
+++ ch04/ch04_5/src/theme/daisyui/ValidatableInput.tsx
@@ -1,14 +1,35 @@
 import type {ReactInputProps} from './Input'
-import {forwardRef, useImperativeHandle, useMemo} from 'react'
+import {forwardRef, useImperativeHandle, useMemo, useRef} from 'react'
 
-export type ValidatableInputMethods = {}
+export type ValidatableInputMethods = {
+  validate: () => [boolean, string]
+}
 
 export const ValidatableInput = forwardRef<ValidatableInputMethods, ReactInputProps>(
-  ({className: _className, ...inputProps}, methodsRef) => {
+  ({type, className: _className, ...inputProps}, methodsRef) => {
     const className = useMemo(() => ['input', _className].join(' '), [_className])
+    const inputRef = useRef<HTMLInputElement>(null)
 
-    useImperativeHandle(methodsRef, () => ({}), [])
+    useImperativeHandle(
+      methodsRef,
+      () => ({
+        validate: (): [boolean, string] => {
+          const value = inputRef.current?.value
+          if (!value || !value.length) return [false, '사용자가 입력한 내용이 없습니다']
 
-    return <input {...inputProps} className={className} />
+          switch (type) {
+            case 'email': {
+              const regEx =
+                /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+              const valid = regEx.test(value)
+              return valid ? [true, value] : [false, '틀린 이메일 주소입니다']
+            }
+          }
+          return [true, value]
+        }
+      }),
+      [type]
+    )
+    return <input {...inputProps} className={className} ref={inputRef} />
   }
 )
