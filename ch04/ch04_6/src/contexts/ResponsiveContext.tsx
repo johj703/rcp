ace0982090de8bf046c8c9a8b620c14bf087a769
Jo <johj703@nate.com>
2023-05-22 18:39:22 +0900

    ResponsiveProvider 컴포넌트 만들기 - ResponsiveProvider 구현하기

--- ch04/ch04_6/src/contexts/ResponsiveContext.tsx
+++ ch04/ch04_6/src/contexts/ResponsiveContext.tsx
@@ -1,3 +1,4 @@
+import type {FC, PropsWithChildren} from 'react'
 import {createContext} from 'react'
 
 type ContextType = {
@@ -7,3 +8,15 @@ const defaultContextValue: ContextType = {
   breakpoint: '' // 공유할 데이터 속성 초기값
 }
 export const ResponsiveContext = createContext<ContextType>(defaultContextValue)
+
+type ResponsiveProviderProps = {}
+export const ResponsiveProvider: FC<PropsWithChildren<ResponsiveProviderProps>> = ({
+  children,
+  ...props
+}) => {
+  const breakpoint = 'sm'
+  const value = {
+    breakpoint // breakpoint: breakpoint 코드를 간결하게 표현
+  }
+  return <ResponsiveContext.Provider value={value} children={children} />
+}
