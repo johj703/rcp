5e05ffaed7e86d74310c09a818f35ea60bb902ce
Jo <johj703@nate.com>
2023-05-23 17:29:22 +0900

    리덕스 기본 개념 이해하기 세팅

---
+++ ch05/ch05_1/src/pages/UseReducerClock.tsx
@@ -0,0 +1,8 @@
+export default function UseReducerClock() {
+  return (
+    <section className="mt-4">
+      <h2 className="text-5xl font-bold text-center">UseReducerClock</h2>
+      <div className="mt-4"></div>
+    </section>
+  )
+}
