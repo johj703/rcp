8b8db30460af2265e322c743a17cb3f3d211b5cc
Jo <johj703@nate.com>
2023-05-23 17:46:03 +0900

    앱 수준 상태 알아보기
    앱 수준 상태 = 여러 컴포넌트가 상태들을 함께 공유하는 형태로 만들 때가 많은데, 이처럼 앱을 구성하는 모든 컴포넌트가 함께 공유할 수 있는 상태

--- ch05/ch05_1/src/App.tsx
+++ ch05/ch05_1/src/App.tsx
@@ -1,11 +1,26 @@
+import type {Action} from 'redux'
+import {Provider as ReduxProvider} from 'react-redux'
+import {configureStore} from '@reduxjs/toolkit'
+
 import ReduxClock from './pages/ReduxClock'
 import UseReducerClock from './pages/UseReducerClock'
 
+type AppState = {
+  today: Date
+}
+const initialAppState = {
+  today: new Date()
+}
+const rootReducer = (state: AppState = initialAppState, action: Action) => state
+const store = configureStore({reducer: rootReducer, middleware: []})
+
 export default function App() {
   return (
-    <main className="p-8">
-      <UseReducerClock />
-      <ReduxClock />
-    </main>
+    <ReduxProvider store={store}>
+      <main className="p-8">
+        <UseReducerClock />
+        <ReduxClock />
+      </main>
+    </ReduxProvider>
   )
 }
