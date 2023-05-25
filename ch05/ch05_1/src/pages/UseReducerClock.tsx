47665daf581477ada6b90aa61a156a85b4d3b725
Jo <johj703@nate.com>
2023-05-25 18:16:27 +0900

    useReducer 훅으로 시계 완성하기

--- ch05/ch05_1/src/pages/UseReducerClock.tsx
+++ ch05/ch05_1/src/pages/UseReducerClock.tsx
@@ -1,8 +1,31 @@
-export default function UseReducerClock() {
+import {useReducer} from 'react'
+import type {AppState} from '../store'
+import type {SetTodayAction} from '../store/actions'
+import {Div, Title, Subtitle} from '../components'
+import {useInterval} from '../hooks'
+
+export default function ReduxClock() {
+  const [{today}, dispatch] = useReducer(
+    (state: AppState, action: SetTodayAction) => {
+      switch (action.type) {
+        case 'setToday':
+          return {...state, today: new Date()}
+      }
+    },
+    {
+      today: new Date()
+    }
+  )
+
+  useInterval(() => {
+    dispatch({type: 'setToday', today: new Date()})
+  })
+
   return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">UseReducerClock</h2>
-      <div className="mt-4"></div>
-    </section>
+    <Div className="flex flex-col items-center justify-center mt-16">
+      <Title className="text-5xl">ReduxClock</Title>
+      <Title className="mt-4 text-3xl">{today.toLocaleTimeString()}</Title>
+      <Subtitle className="mt-4 text-2xl">{today.toLocaleDateString()}</Subtitle>
+    </Div>
   )
 }
