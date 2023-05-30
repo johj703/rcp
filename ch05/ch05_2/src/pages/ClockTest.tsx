b1c7da485ef4ca48216ff487f4a17b5737705c46
Jo <johj703@nate.com>
2023-05-30 15:29:40 +0900

    시계 만들기 - 시계 테스트 코드 작성하기

--- ch05/ch05_2/src/pages/ClockTest.tsx
+++ ch05/ch05_2/src/pages/ClockTest.tsx
@@ -1,8 +1,21 @@
-export default function CopyMe() {
+import {useSelector, useDispatch} from 'react-redux'
+import {Title} from '../components'
+import {useInterval} from '../hooks'
+import type {AppState} from '../store'
+import * as C from '../store/clock'
+
+export default function ClockTest() {
+  const clock = useSelector<AppState, C.State>((state) => state.clock)
+  const dispatch = useDispatch()
+  useInterval(() => dispatch(C.setClock(new Date())))
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>ClockTest</Title>
+      <div className="flex flex-col items-center mt-4">
+        <p className="text-2xl text-blue-600 text-bold">{clock.toLocaleTimeString()}</p>
+        <p className="text-lg text-blue-400 text-bold">{clock.toLocaleDateString()}</p>
+      </div>
     </section>
   )
 }
