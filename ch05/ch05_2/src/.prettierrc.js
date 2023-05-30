0ab51be72110eb017758579ad04dafa494fb8548
Jo <johj703@nate.com>
2023-05-30 14:20:51 +0900

    리듀서 활용하기 제거

--- ch05/ch05_2/src/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'none',
-  allowParens: 'avoid',
-  semi: false,
-  printWidth: 90,
-}
