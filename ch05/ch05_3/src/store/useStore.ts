2f688457a1b0f598c4f21f386121c1f0e8b760e7
Jo <johj703@nate.com>
2023-06-01 15:23:42 +0900

    미들웨어 설정하기 - 개발 모드일 때에만 기록하도록 설정하기

--- ch05/ch05_3/src/store/useStore.ts
+++ ch05/ch05_3/src/store/useStore.ts
@@ -1,9 +1,17 @@
 import {configureStore} from '@reduxjs/toolkit'
 import {useMemo} from 'react'
 import {rootReducer} from './rootReducer'
+import logger from './logger'
+
+const useLogger = process.env.NODE_ENV !== 'production'
 
 const initializeStore = () => {
-  const store = configureStore({reducer: rootReducer, middleware: []})
+  const middleware: any[] = []
+  if (useLogger) {
+    middleware.push(logger)
+  }
+
+  const store = configureStore({reducer: rootReducer, middleware})
   return store
 }
 
