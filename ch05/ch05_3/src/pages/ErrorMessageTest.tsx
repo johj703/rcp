87c4624a0eafd07045bb8e16ba1d03f3dfdd6033
Jo <johj703@nate.com>
2023-06-07 20:27:37 +0900

    오류 메시지 구현하기 - 오류 메시지 썽크 액션 테스트하기

--- ch05/ch05_3/src/pages/ErrorMessageTest.tsx
+++ ch05/ch05_3/src/pages/ErrorMessageTest.tsx
@@ -1,8 +1,34 @@
-export default function CopyMe() {
+import {useCallback} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import type {AppState} from '../store'
+import {Title} from '../components'
+import {Button} from '../theme/daisyui'
+import * as D from '../data'
+import * as E from '../store/errorMessage'
+
+export default function ErrorMessageTest() {
+  const dispatch = useDispatch()
+  const errorMessage = useSelector<AppState, E.State>(({errorMessage}) => errorMessage)
+
+  const generateErrorMessage = useCallback(() => {
+    dispatch<any>(E.generateErrorMessage(D.randomSentence()))
+  }, [dispatch])
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>ErrorMessageTest</Title>
+      <div className="mt-4">
+        <div className="flex justify-center mt-4">
+          <Button className="btn btn-sm btn-primary" onClick={generateErrorMessage}>
+            generate error message
+          </Button>
+        </div>
+        {errorMessage.length && (
+          <div className="flex items-center justify-center bg-red-200">
+            <p className="text-2xl text-red-600 text-bold">error: {errorMessage}</p>
+          </div>
+        )}
+      </div>
     </section>
   )
 }
