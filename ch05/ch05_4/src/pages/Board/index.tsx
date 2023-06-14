f1720710fb856d32cffcdbf8d940dbc4df709a4d
Jo <johj703@nate.com>
2023-06-14 11:32:35 +0900

    listidOrders와 listEntities 적용한 보드 수정하기

--- ch05/ch05_4/src/pages/Board/index.tsx
+++ ch05/ch05_4/src/pages/Board/index.tsx
@@ -1,11 +1,22 @@
 import {useCallback} from 'react'
+import {useDispatch} from 'react-redux'
 import {Title} from '../../components'
 import CreateListForm from './CreateListForm'
 
+import * as L0 from '../../store/listidOrders'
+import * as L from '../../store/listEntities'
+
 export default function Board() {
-  const onCreateList = useCallback((uuid: string, title: string) => {
-    console.log('onCreateList', uuid, title)
-  }, [])
+  const dispatch = useDispatch()
+
+  const onCreateList = useCallback(
+    (uuid: string, title: string) => {
+      const list = {uuid, title}
+      dispatch(L0.addListidToOrders(list.uuid))
+      dispatch(L.addList(list))
+    },
+    [dispatch]
+  )
   return (
     <section className="mt-4">
       <Title>Board</Title>
