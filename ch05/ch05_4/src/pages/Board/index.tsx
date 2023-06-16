ba30eccb05087423b5a1214cc572430b90ac4d62
Jo <johj703@nate.com>
2023-06-16 14:52:14 +0900

    리덕스 기능을 커스텀 훅으로 만들기 - useLists 훅으로 간결해진 Board 컴포넌트

--- ch05/ch05_4/src/pages/Board/index.tsx
+++ ch05/ch05_4/src/pages/Board/index.tsx
@@ -1,37 +1,12 @@
-import {useCallback, useMemo} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
+import {useMemo} from 'react'
 import {Title} from '../../components'
 import CreateListForm from './CreateListForm'
 import BoardList from '../BoardList'
 
-import type {AppState} from '../../store'
-import type {List} from '../../store/commonTypes'
-import * as L0 from '../../store/listidOrders'
-import * as L from '../../store/listEntities'
+import {useLists} from '../../store/useLists'
 
 export default function Board() {
-  const dispatch = useDispatch()
-
-  const lists = useSelector<AppState, List[]>(({listidOrders, listEntities}) =>
-    listidOrders.map((uuid) => listEntities[uuid])
-  )
-
-  const onCreateList = useCallback(
-    (uuid: string, title: string) => {
-      const list = {uuid, title}
-      dispatch(L0.addListidToOrders(uuid))
-      dispatch(L.addList(list))
-    },
-    [dispatch]
-  )
-
-  const onRemoveList = useCallback(
-    (listid: string) => () => {
-      dispatch(L.removeList(listid))
-      dispatch(L0.removeListidFromOrders(listid))
-    },
-    [dispatch]
-  )
+  const {lists, onRemoveList, onCreateList} = useLists()
 
   const children = useMemo(
     () =>
@@ -43,7 +18,7 @@ export default function Board() {
   return (
     <section className="mt-4">
       <Title>Board</Title>
-      <div className="flex p-2 mt-4 flrx-wrap">
+      <div className="flex flex-wrap p-2 mt-4">
         {children}
         <CreateListForm onCreateList={onCreateList} />
       </div>
