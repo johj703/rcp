dfbef2a1e934f648af06a7074e32e490ba849ea8
HyunJun-Jo <johj703@nate.com>
2023-06-16 20:37:48 +0900

    Revert "cardEntities 멤버 상태 구현하기"
    This reverts commit dbee8e33226587ba3d7a7c7b58c0da0798af938c.

--- ch05/ch05_4/src/store/cardEntities/types.ts
+++ ch05/ch05_4/src/store/cardEntities/types.ts
@@ -1,15 +1,4 @@
 import type {Action} from 'redux'
-import type {Card, UUID} from '../commonTypes'
-export * from '../commonTypes'
 
-export type State = Record<UUID, Card>
-
-export type AddCardAction = Action<'@cardEntities/add'> & {
-  payload: Card
-}
-
-export type RemoveCardAction = Action<'@cardEntities/remove'> & {
-  payload: UUID
-}
-
-export type Actions = AddCardAction | RemoveCardAction
+export type State = any
+export type Actions = Action
