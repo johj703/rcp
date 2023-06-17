5616e3cc401ddbde3f66ee060dd326e568003af0
Jo <johj703@nate.com>
2023-06-17 16:14:25 +0900

    listidCardidOrders 멤버 상태 구현하기 - useLIsts 훅에 반영하기

--- ch05/ch05_4/src/store/useLists.ts
+++ ch05/ch05_4/src/store/useLists.ts
@@ -4,6 +4,8 @@ import type {AppState} from '../store'
 import type {List} from '../store/commonTypes'
 import * as L0 from '../store/listidOrders'
 import * as L from '../store/listEntities'
+import * as C from '../store/cardEntities'
+import * as LC from '../store/listidCardidOrders'
 
 export const useLists = () => {
   const dispatch = useDispatch()
@@ -11,21 +13,30 @@ export const useLists = () => {
   const lists = useSelector<AppState, List[]>(({listidOrders, listEntities}) =>
     listidOrders.map((uuid) => listEntities[uuid])
   )
+  const listidCardidOrders = useSelector<AppState, LC.State>(
+    ({listidCardidOrders}) => listidCardidOrders
+  )
 
   const onCreateList = useCallback(
     (uuid: string, title: string) => {
       const list = {uuid, title}
       dispatch(L0.addListidToOrders(uuid))
       dispatch(L.addList(list))
+      dispatch(LC.setListidCardids({listid: list.uuid, cardids: []}))
     },
     [dispatch]
   )
   const onRemoveList = useCallback(
     (listid: string) => () => {
+      listidCardidOrders[listid].forEach((cardid) => {
+        dispatch(C.removeCard(cardid))
+      })
+      dispatch(LC.removeListid(listid))
+
       dispatch(L.removeList(listid))
       dispatch(L0.removeListidFromOrders(listid))
     },
-    [dispatch]
+    [dispatch, listidCardidOrders]
   )
   return {lists, onCreateList, onRemoveList}
 }
