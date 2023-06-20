b6468f6e619bc0cfcfe6890f1416f46d92fcf75c
Jo <johj703@nate.com>
2023-06-20 11:50:14 +0900

    Revert "CardDraggable 컴포넌트 구현하기 - CardDraggable 컴포넌트 작성하기"
    This reverts commit cb9dcab4f66d720dc3735fd1d114b8e1ec1bc8e3.

--- ch05/ch05_4/src/pages/Board/index.tsx
+++ ch05/ch05_4/src/pages/Board/index.tsx
@@ -1,5 +1,4 @@
 import {useMemo} from 'react'
-import {DragDropContext} from 'react-beautiful-dnd'
 import {Title} from '../../components'
 import CreateListForm from './CreateListForm'
 import BoardList from '../BoardList'
@@ -8,7 +7,7 @@ import {ListDroppable} from '../../components'
 import {useLists} from '../../store/useLists'
 
 export default function Board() {
-  const {lists, onRemoveList, onCreateList, onMoveList, onDragEnd} = useLists()
+  const {lists, onRemoveList, onCreateList, onMoveList} = useLists()
 
   const children = useMemo(
     () =>
@@ -26,14 +25,12 @@ export default function Board() {
   return (
     <section className="mt-4">
       <Title>Board</Title>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <ListDroppable className="flex flex-row p-2 mt-4">
-          <div className="flex flex-wrap p-2 mt-4">
-            {children}
-            <CreateListForm onCreateList={onCreateList} />
-          </div>
-        </ListDroppable>
-      </DragDropContext>
+      <ListDroppable className="flex flex-row p-2 mt-4">
+        <div className="flex flex-wrap p-2 mt-4">
+          {children}
+          <CreateListForm onCreateList={onCreateList} />
+        </div>
+      </ListDroppable>
     </section>
   )
 }
