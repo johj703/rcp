6bd47d5cb48226fad3ac8a2be952f35df7c842ca
Jo <johj703@nate.com>
2023-06-23 19:25:34 +0900

    NoMatch 컴포넌트 만들기 - 라우트 설정하기 초기

--- ch06/ch06_1/src/routes/RoutesSetup.tsx
+++ ch06/ch06_1/src/routes/RoutesSetup.tsx
@@ -1,8 +1,10 @@
+import {Routes, Route} from 'react-router-dom'
+import NoMatch from './NoMatch'
+
 export default function RoutesSetup() {
   return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">RoutesSetup</h2>
-      <div className="mt-4"></div>
-    </section>
+    <Routes>
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
   )
 }
