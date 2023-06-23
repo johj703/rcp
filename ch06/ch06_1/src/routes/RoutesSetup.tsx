c269a5b71fb15ff31bd342999cdee33675ab683a
Jo <johj703@nate.com>
2023-06-23 18:55:11 +0900

    처음 만나는 리액트 라우터 세팅

---
+++ ch06/ch06_1/src/routes/RoutesSetup.tsx
@@ -0,0 +1,8 @@
+export default function RoutesSetup() {
+  return (
+    <section className="mt-4">
+      <h2 className="text-5xl font-bold text-center">RoutesSetup</h2>
+      <div className="mt-4"></div>
+    </section>
+  )
+}
