92bb92343dfc71fe51d638b514f5d68c1219d7a3
Jo <johj703@nate.com>
2023-06-24 13:19:10 +0900

    Home 컴포넌트 만들기 - 라우트 설정에 경로 '/'와 '/welcome' 추가하기

--- ch06/ch06_1/src/routes/RoutesSetup.tsx
+++ ch06/ch06_1/src/routes/RoutesSetup.tsx
@@ -1,9 +1,12 @@
 import {Routes, Route} from 'react-router-dom'
 import NoMatch from './NoMatch'
+import Home from './Home'
 
 export default function RoutesSetup() {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/welcome" element={<Home title="welcome to our site" />} />
       <Route path="*" element={<NoMatch />} />
     </Routes>
   )
