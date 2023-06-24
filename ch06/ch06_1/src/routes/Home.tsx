58cb8ec6fc80c08688385851d31cea82296da3c6
Jo <johj703@nate.com>
2023-06-24 13:16:13 +0900

    Revert "Home 컴포넌트 작성하기 초기"
    This reverts commit e7d4c452b94251940060085d20e9f4789764603f.

--- ch06/ch06_1/src/routes/Home.tsx
+++ ch06/ch06_1/src/routes/Home.tsx
@@ -1,10 +1,8 @@
-import type {FC} from 'react'
-
-type HomeProps = {
-  title?: string
+export default function Home() {
+  return (
+    <section className="mt-4">
+      <h2 className="text-5xl font-bold text-center">Home</h2>
+      <div className="mt-4"></div>
+    </section>
+  )
 }
-
-const Home: FC<HomeProps> = ({title}) => {
-  return <p>{title ?? 'Home'}</p>
-}
-export default Home
