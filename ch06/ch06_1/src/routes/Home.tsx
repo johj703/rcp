6138a84d7fa0a41c1c474535091661acc71ae518
Jo <johj703@nate.com>
2023-06-24 14:01:14 +0900

    Link 컴포넌트 알아보기 - Home 컴포넌트 수정하기(Link 컴포넌트 사용)

--- ch06/ch06_1/src/routes/Home.tsx
+++ ch06/ch06_1/src/routes/Home.tsx
@@ -1,10 +1,24 @@
 import type {FC} from 'react'
+import {Link} from 'react-router-dom'
 
 type HomeProps = {
   title?: string
 }
 
 const Home: FC<HomeProps> = ({title}) => {
-  return <p>{title ?? 'Home'}</p>
+  return (
+    <div>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/welcome" className="ml-4">
+          Welcome
+        </Link>
+        <Link to="/board" className="ml-4">
+          Board
+        </Link>
+      </div>
+      <p className="text-xl text-center text-bold">{title ?? 'Home'}</p>
+    </div>
+  )
 }
 export default Home
