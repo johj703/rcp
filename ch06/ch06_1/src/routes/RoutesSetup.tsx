01ba4aaba766b1db5079d15db7a3076355fe52ce
Jo <johj703@nate.com>
2023-06-25 20:18:17 +0900

    Link 컴포넌트 알아보기 - board 경로일 때, Board 컴포넌트 보이기

--- ch06/ch06_1/src/routes/RoutesSetup.tsx
+++ ch06/ch06_1/src/routes/RoutesSetup.tsx
@@ -1,12 +1,14 @@
 import {Routes, Route} from 'react-router-dom'
 import NoMatch from './NoMatch'
 import Home from './Home'
+import Board from '../pages/Board'
 
 export default function RoutesSetup() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/welcome" element={<Home title="welcome to our site" />} />
+      <Route path="/Board" element={<Board />} />
       <Route path="*" element={<NoMatch />} />
     </Routes>
   )
