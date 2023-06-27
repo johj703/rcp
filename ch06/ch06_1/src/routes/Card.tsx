96c93dac56a54ce9df9c8908a5ed1382a1805fb4
Jo <johj703@nate.com>
2023-06-27 21:24:46 +0900

    Card 컴포넌트에 리액트 라우터 훅 적용하기 - Card 컴포넌트에 리액트 라우터 훅 적용하기 - Card 컴포넌트 수정하기(리액트 라우터 훅 반영)

--- ch06/ch06_1/src/routes/Card.tsx
+++ ch06/ch06_1/src/routes/Card.tsx
@@ -1,16 +1,22 @@
 import {useCallback} from 'react'
-import {useLocation, useNavigate} from 'react-router-dom'
+import {useLocation, useParams, useNavigate, useSearchParams} from 'react-router-dom'
 
 export default function Card() {
   const location = useLocation()
-
+  const params = useParams()
   const navigate = useNavigate()
+  const [search] = useSearchParams()
   const goBack = useCallback(() => {
     navigate(-1)
   }, [navigate])
   return (
-    <div>
+    <div className="p-4">
       <p>location: {JSON.stringify(location, null, 2)}</p>
+      <p>params: {JSON.stringify(params, null, 2)}</p>
+      <p>cardid: {params['cardid']}</p>
+      <p>
+        from: {search.get('from')}, to: {search.get('to')}
+      </p>
       <button className="mt-4 btn btn-primary btn-xs" onClick={goBack}>
         go back
       </button>
