6b56e8e0b069e22311f6fdfe405a3eda8eb48e4b
Jo <johj703@nate.com>
2023-06-27 20:40:33 +0900

    Card 컴포넌트 만들기 - 라우트 설정하기(/board/card 경로 추가)

--- ch06/ch06_1/src/routes/RoutesSetup.tsx
+++ ch06/ch06_1/src/routes/RoutesSetup.tsx
@@ -2,6 +2,7 @@ import {Routes, Route} from 'react-router-dom'
 import NoMatch from './NoMatch'
 import Home from './Home'
 import Board from '../pages/Board'
+import Card from './Card'
 
 export default function RoutesSetup() {
   return (
@@ -9,6 +10,7 @@ export default function RoutesSetup() {
       <Route path="/" element={<Home />} />
       <Route path="/welcome" element={<Home title="welcome to our site" />} />
       <Route path="/Board" element={<Board />} />
+      <Route path="/Board/card/:cardid" element={<Card />} />
       <Route path="*" element={<NoMatch />} />
     </Routes>
   )
