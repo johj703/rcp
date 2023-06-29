630695016ba3c129b4c91d1de1a51abe3b7ea858
Jo <johj703@nate.com>
2023-06-29 22:09:52 +0900

    리액트 라우터의 Outlet 컴포넌트 - 라우트 설정하기(Layout 적용)

--- ch06/ch06_2/src/routes/RoutesSetup.tsx
+++ ch06/ch06_2/src/routes/RoutesSetup.tsx
@@ -1,10 +1,15 @@
 import {Routes, Route} from 'react-router-dom'
+import Layout from './Layout'
+import Board from '../pages/Board'
 import NoMatch from './NoMatch'
 
 export default function RoutesSetup() {
   return (
     <Routes>
-      <Route path="*" element={<NoMatch />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/board" element={<Board />} />
+        <Route path="*" element={<NoMatch />} />
+      </Route>
     </Routes>
   )
 }
