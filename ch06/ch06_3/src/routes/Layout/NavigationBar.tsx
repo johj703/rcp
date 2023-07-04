90b2dfbdfb0d3e8299b6ee00e186212ff4ccc670
Jo <johj703@nate.com>
2023-07-04 17:44:53 +0900

    로그인 여부에 따라 내비게이션 메뉴 구분하기 - NavigationBar 컴포넌트 작성하기 초기

--- ch06/ch06_3/src/routes/Layout/NavigationBar.tsx
+++ ch06/ch06_3/src/routes/Layout/NavigationBar.tsx
@@ -1,15 +1,28 @@
-// import {Link} from 'react-router-dom'
+import {Link as RRLink} from 'react-router-dom'
 import {Link} from '../../components'
 
 export default function NavigationBar() {
   return (
-    <div className="flex p-4 bg-gray-200">
-      <Link to="/" className="btn btn-link">
-        Home
-      </Link>
-      <Link to="/board" className="ml-4 btn btn-link">
-        Board
-      </Link>
+    <div className="flex justify-between bg-base-100">
+      <div className="flex p-2 navbar">
+        <Link to="/" className="btn btn-link">
+          Home
+        </Link>
+        <Link to="/board" className="ml-4 btn btn-link">
+          Board
+        </Link>
+      </div>
+      <div className="flex items-center p-2">
+        <RRLink to="/login" className="btn btn-sm btn-primary">
+          Login
+        </RRLink>
+        <RRLink to="/signup" className="ml-4 btn btn-sm btn-outline btn-primary">
+          Signup
+        </RRLink>
+        <RRLink to="/logout" className="ml-4 mr-4">
+          LOGOUT
+        </RRLink>
+      </div>
     </div>
   )
 }
