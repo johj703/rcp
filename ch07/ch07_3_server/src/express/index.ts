a167baedafee655cb9ae479d795a6b84b9838bc9
Jo <johj703@nate.com>
2023-07-09 13:08:07 +0900

    익스프레스 미들웨어와 use 메서드 - 로깅 미들웨어 추가하기

--- ch07/ch07_3_server/src/express/index.ts
+++ ch07/ch07_3_server/src/express/index.ts
@@ -1,8 +1,16 @@
 import express from 'express'
+import {appendFile} from 'fs'
 
 export const createExpressApp = (...args: any[]) => {
-  const app = express().get('/', (req, res) => {
-    res.json({message: 'Hello express World!'})
-  })
+  const app = express()
+  app
+    .use((req, res, next) => {
+      console.log(`url='${req.url}, method=${req.method}`)
+      next()
+    })
+    .get('/', (req, res) => {
+      res.json({message: 'Hello express World!'})
+    })
+
   return app
 }
