81ddbfa9948b55a9ba69e19086c05b38f6f1b251
Jo <johj703@nate.com>
2023-07-10 12:59:51 +0900

    express.static 미들웨어 - 파일 서버 기능 추가하기
    express.static이란 미들웨어를 사용해서 익스프레스 객체가 public 디렉토리에 있는 .html, .css, .js, .png와 같은 파일을 웹 브라우저에 응답할 수 있게 하는 정적 파일 서버로 동작할 수 있게 함.

--- ch07/ch07_3_server/src/express/index.ts
+++ ch07/ch07_3_server/src/express/index.ts
@@ -1,13 +1,14 @@
 import express from 'express'
-import {appendFile} from 'fs'
 
 export const createExpressApp = (...args: any[]) => {
   const app = express()
+
   app
     .use((req, res, next) => {
       console.log(`url='${req.url}, method=${req.method}`)
       next()
     })
+    .use(express.static('public'))
     .get('/', (req, res) => {
       res.json({message: 'Hello express World!'})
     })
