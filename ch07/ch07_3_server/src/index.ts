16f903b1a9ac54159155eb2910ac40d79aa0cc77
Jo <johj703@nate.com>
2023-07-12 07:22:56 +0900

    몽고DB 연결하기 - 몽고DB 연결 추가하기

--- ch07/ch07_3_server/src/index.ts
+++ ch07/ch07_3_server/src/index.ts
@@ -1,14 +1,18 @@
 import {createServer} from 'http'
 import {getPublicDirPath} from './config'
 import {makeDir} from './utils'
-
 import {createExpressApp} from './express'
+import type {MongoDB} from './mongodb'
+import {connectAndUseDB} from './mongodb'
 
 makeDir(getPublicDirPath())
 
-const hostname = 'localhost',
-  port = 4000
+const connectCallback = (db: MongoDB) => {
+  const hostname = 'localhost',
+    port = 4000
 
-createServer(createExpressApp()).listen(port, () =>
-  console.log(`connect http://${hostname}:${port}`)
-)
+  createServer(createExpressApp(db)).listen(port, () =>
+    console.log(`connect http://${hostname}:${port}`)
+  )
+}
+connectAndUseDB(connectCallback, 'ch07')
