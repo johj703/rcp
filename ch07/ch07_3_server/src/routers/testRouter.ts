c663bb2505d20d0e9a39e2ea4358be8c22ce715e
Jo <johj703@nate.com>
2023-07-13 15:05:27 +0900

    몽고DB 연결하기 - 몽고DB 기능 추가하기

--- ch07/ch07_3_server/src/routers/testRouter.ts
+++ ch07/ch07_3_server/src/routers/testRouter.ts
@@ -1,31 +1,69 @@
+import type {MongoDB} from '../mongodb'
 import {Router} from 'express'
 
 export const testrouter = (...args: any[]) => {
+  const db: MongoDB = args[0]
+  const test = db.collection('test')
   const router = Router()
   return router
-    .get('/', (req, res) => {
-      //모든 데이터를 요청하는 경우
-      res.json({ok: true})
+    .get('/', async (req, res) => {
+      try {
+        const findResult = await test.find({}).toArray()
+        res.json({ok: true, body: findResult})
+      } catch (e) {
+        if (e instanceof Error) res.json({ok: false, errorMessage: e.message})
+      }
     })
-    .get('/:id', (req, res) => {
+    .get('/:id', async (req, res) => {
       const {id} = req.params
-      // id값을 가진 데이터만 요청하는 경우
-      res.json({ok: true, id})
+      try {
+        const findResult = await test.findOnd({id})
+        res.json({ok: true, body: findResult})
+      } catch (e) {
+        if (e instanceof Error) res.json({ok: false, errorMessage: e.message})
+      }
     })
-    .post('/', (req, res) => {
-      // req.body의 데이터를 서버에 저장하기를 요청하는 경우
+    .post('/', async (req, res) => {
       const {body} = req
-      res.json({ok: true, body})
+      try {
+        try {
+          await test.drop() // 항상 id: '1234'인 문서가 단 하나만 있도록
+        } catch (e) {
+          /* 오류 무시 */
+        }
+
+        // 과거 문서를 모두 지움(보통은 필요 없는 코드)
+        const insertResult = await test.insertOne({id: '1234', ...body})
+        const {insertedId} = insertResult
+        const findResult = await test.findOne({_id: insertedId})
+        res.json({ok: true, body: findResult})
+      } catch (e) {
+        if (e instanceof Error) res.json({ok: false, errorMessage: e.message})
+      }
     })
-    .put('/:id', (req, res) => {
-      // id값을 가진 데이터의 수정을 요청하는 경우
+    .put('/:id', async (req, res) => {
       const {id} = req.params
       const {body} = req
-      res.json({ok: true, body, id})
+      try {
+        const updateResult = await test.findOneAndUpdate(
+          {id},
+          {$set: body},
+          {
+            returnDocument: 'after'
+          }
+        )
+        res.json({ok: true, body: updateResult.value})
+      } catch (e) {
+        if (e instanceof Error) res.json({ok: false, errorMessage: e.message})
+      }
     })
-    .delete('/:id', (req, res) => {
-      // id값을 가진 데이터의 삭제를 요청하는 경우
+    .delete('/:id', async (req, res) => {
       const {id} = req.params
-      res.json({ok: true, id})
+      try {
+        await test.deleteOne({id})
+        res.json({ok: true})
+      } catch (e) {
+        if (e instanceof Error) res.json({ok: false, errorMessage: e.message})
+      }
     })
 }
