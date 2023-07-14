9482a6c307db4b66978fe82c6f7ab1f2d2a207a4
Jo <johj703@nate.com>
2023-07-14 14:53:29 +0900

    Revert "fetch 함수로 JSON 형식 데이터 가져오기"
    This reverts commit c9e2bd0c97431dd6816375923724eef4373a044b.

--- ch07/ch07_3_client/src/routes/RestTest/GetTest.tsx
+++ ch07/ch07_3_client/src/routes/RestTest/GetTest.tsx
@@ -1,22 +1,8 @@
-import {useState, useEffect} from 'react'
-
-export default function GetTest() {
-  const [data, setData] = useState<object>({})
-  const [errorMessage, setErrorMessage] = useState<string | null>(null)
-
-  useEffect(() => {
-    fetch('http://localhost:4000/test')
-      .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch((error) => setErrorMessage(error.message))
-  }, [])
+export default function CopyMe() {
   return (
-    <div>
-      <p className="text-2xl text-center text-bold">GetTest</p>
-      <div className="mt-4 text-center">
-        <p>data: {JSON.stringify(data, null, 2)}</p>
-        {errorMessage && <p>error: {errorMessage}</p>}
-      </div>
-    </div>
+    <section className="mt-4">
+      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
+      <div className="mt-4"></div>
+    </section>
   )
 }
