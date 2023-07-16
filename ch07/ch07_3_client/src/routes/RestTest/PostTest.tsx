c78db2f61ba25e2d73db53eebacbf70fdf4838dd
Jo <johj703@nate.com>
2023-07-16 14:01:55 +0900

    컬렉션에 데이터 저장하기

--- ch07/ch07_3_client/src/routes/RestTest/PostTest.tsx
+++ ch07/ch07_3_client/src/routes/RestTest/PostTest.tsx
@@ -1,8 +1,29 @@
-export default function CopyMe() {
+import {useState, useCallback} from 'react'
+import {post} from '../../server'
+import * as D from '../../data'
+
+export default function PostTest() {
+  const [data, setData] = useState<object>({})
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
+
+  const postTest = useCallback(() => {
+    post('/test', D.makeRandomCard())
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((error) => setErrorMessage(error.message))
+  }, [])
+
   return (
-    <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">CopyMe</h2>
-      <div className="mt-4"></div>
-    </section>
+    <div className="mb-4">
+      <div className="flex justify-center mb-4">
+        <button onClick={postTest} className="btn btn-primary">
+          POST
+        </button>
+      </div>
+      <div className="mt-4 text-center">
+        <p>data: {JSON.stringify(data, null, 2)}</p>
+        {errorMessage && <p>error: {errorMessage}</p>}
+      </div>
+    </div>
   )
 }
